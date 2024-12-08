fc6d0f4069f3a942e616fb4dbcfdd0f08244aa81
Matheus Cavalcante <mc2798353@gmail.com>
2024-12-08 11:33:00 -0300

    Fix: ajustando migration para criar os profiles na hr de subir, e ajustando o HospitalService de pasta

--- database/migrations/2024_12_08_060230_create_profiles_table.php
+++ database/migrations/2024_12_08_060230_create_profiles_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -16,6 +17,12 @@ return new class extends Migration
             $table->string('description')->unique(); // Exemplo: Administrador, Receptor, Doador
             $table->timestamps();
         });
+        // Adicionando os registros iniciais
+        DB::table('profiles')->insert([
+            ['description' => 'Administrador', 'created_at' => now(), 'updated_at' => now()],
+            ['description' => 'Receptor', 'created_at' => now(), 'updated_at' => now()],
+            ['description' => 'Doador', 'created_at' => now(), 'updated_at' => now()],
+        ]);
     }
 
     /**
